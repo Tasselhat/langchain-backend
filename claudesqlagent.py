ee69aa67a09ea63c514f768c41e15f74b3167267
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2024-03-14 00:36:56 -0700

    claude sql agent

--- claudesqlagent.py
+++ claudesqlagent.py
@@ -1,23 +1,28 @@
+import os
+
+from anthropic import Anthropic
 from dotenv import find_dotenv, load_dotenv
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_community.utilities import SQLDatabase
-from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)  # load api key
 # Uncomment the below to use LangSmith. Not required.
 # os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
 # os.environ["LANGCHAIN_TRACING_V2"] = "true"
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)  # load api key
+
+API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+
 
 db = SQLDatabase.from_uri(
     "mysql+pymysql://root:Peanutbutter11@localhost:3306/goals_app")
-
-# Point to the local server
-llm = ChatOpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
-
+llm = ChatAnthropic(
+    model="claude-3-opus-20240229"
+)
 
 agent_executor = create_sql_agent(
-    llm, db=db, agent_type="openai-tools", verbose=True)
+    llm, db=db, agent_type="zero-shot-react-description", verbose=True)
 agent_executor.invoke(
     {
         "input": "Count the total number of rows in each table in the database"
