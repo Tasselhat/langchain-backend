3ec9c2407bc11d5843da415e580ece4bb5992e5f
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2024-04-02 18:14:15 -0700

    website augmented rag chatbot

--- index.py
+++ index.py
@@ -1,20 +1,17 @@
 # pip install langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
-from flask import jsonify, request
-from flask_cors import cross_origin
-
-
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
+from langchain.chains import (create_history_aware_retriever,
+                              create_retrieval_chain)
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 app = Flask(__name__)
 cors = CORS(app, headers='Content-Type')
@@ -69,18 +66,48 @@ def get_conversational_rag_chain(retriever_chain):
     return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
 
-@app.route('/chat/url', methods=['POST'])
+def get_conversational_chain_no_rag():
+    output_parser = StrOutputParser()
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    chain = prompt | llm | output_parser
+
+    return chain
+
+
+@app.route('/chat', methods=['POST'])
 @cross_origin()
 def get_response():
     data = request.get_json()
-    website_url = data['website_url']
-    vector_store = get_vectorstore_from_url(website_url)
-    retriever_chain = get_context_retriever_chain(vector_store)
-    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    website_url = data['websiteURL']
+    if website_url:
+        vector_store = get_vectorstore_from_url(website_url)
+        retriever_chain = get_context_retriever_chain(vector_store)
+        conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
 
-    response = conversation_rag_chain.invoke({
-        "chat_history": data['chat_history'],
-        "input": data['input']
-    })
+        response = conversation_rag_chain.invoke({
+            "chat_history": data['messages'],
+            "input": data['input']
+        })
 
-    return jsonify(response['answer'])
+        print(response)
+
+        return jsonify(response['answer'])
+    else:
+        chain = get_conversational_chain_no_rag()
+
+        response = chain.invoke({
+            "chat_history": data['messages'],
+            "input": data['input']
+        })
+
+        return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    print("Running on port 5000")
