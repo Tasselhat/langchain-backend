50278e17982505b7a2dcf9840e3fbbecd901ec58
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2024-04-03 15:02:32 -0700

    routes moved to app.py

--- app.py
+++ app.py
@@ -1,21 +1,84 @@
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
-from flask import jsonify, request
-from flask_cors import cross_origin
-
 from dotenv import find_dotenv, load_dotenv
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from langchain.chains import (create_history_aware_retriever,
+                              create_retrieval_chain)
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.utilities import SQLDatabase
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS, Chroma
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import (ChatPromptTemplate, FewShotPromptTemplate,
                                     MessagesPlaceholder, PromptTemplate,
                                     SystemMessagePromptTemplate)
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-
 app = Flask(__name__)
 cors = CORS(app, headers='Content-Type')
+
+
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
+
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+
+    retriever = vector_store.as_retriever()
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+
+def get_conversational_rag_chain(retriever_chain):
+
+    llm = ChatOpenAI()
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system",
+         "Answer the user's questions based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+
+def get_conversational_chain_no_rag():
+    output_parser = StrOutputParser()
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    chain = prompt | llm | output_parser
+
+    return chain
 
 
 def agent(input):
@@ -130,6 +193,35 @@ def process_agent_message():
     data = request.get_json()
     print(data)
     return jsonify(agent(data['input']))
+
+
+@app.route('/websitechat', methods=['POST'])
+@cross_origin()
+def get_response():
+    data = request.get_json()
+    website_url = data['websiteURL']
+    if website_url:
+        vector_store = get_vectorstore_from_url(website_url)
+        retriever_chain = get_context_retriever_chain(vector_store)
+        conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+        response = conversation_rag_chain.invoke({
+            "chat_history": data['messages'],
+            "input": data['input']
+        })
+
+        print(response)
+
+        return jsonify(response['answer'])
+    else:
+        chain = get_conversational_chain_no_rag()
+
+        response = chain.invoke({
+            "chat_history": data['messages'],
+            "input": data['input']
+        })
+
+        return jsonify(response)
 
 
 if __name__ == "__main__":
